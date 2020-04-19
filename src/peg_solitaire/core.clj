c9c3a61004c6a84124d8000fb8d3ccd46cc9f8ca
kantuni <henrikh.kantuni@gmail.com>
2020-04-20 00:03:40 +0400

    Part 1

--- src/peg_solitaire/core.clj
+++ src/peg_solitaire/core.clj
@@ -1,7 +1,49 @@
 (ns peg-solitaire.core
+  (:require [clojure.set :as set])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(declare successful-move prompt-move game-over query-rows)
+
+(defn tri*
+  "Generates lazy sequence of triangular numbers"
+  ([] (tri* 0 1))
+  ([sum n]
+   (let [new-sum (+ sum n)]
+     (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+
+(def tri (tri*))
+
+(defn triangular?
+  "Checks if the number is triangular (i.e. 1, 3, 6, ...)"
+  [n]
+  (= n (last (take-while #(>= n %) tri))))
+
+(defn row-tri
+  "Returns the triangular number at the end of row n"
+  [n]
+  (last (take n tri)))
+
+(defn row-num
+  "Returns row number of the position"
+  [pos]
+  (inc (count (take-while #(> pos %) tri))))
+
+(defn connect
+  "Form a mutual connection between two positions"
+  [board max-pos pos neighbor destination]
+  (if (<= destination max-pos)
+    (reduce (fn [new-board [p1 p2]]
+              (assoc-in new-board [p1 :connections p2] neighbor))
+            board
+            [[pos destination] [destination pos]])
+    board))
+
+(defn connect-right
+  "Form a mutual connection between the given position and 
+  the one to it's right"
+  [board max-pos pos]
+  (let [neighbor (inc pos)
+        destination (inc neighbor)]
+    (if-not (or (triangular? neighbor) (triangular? pos))
+      (connect board max-pos pos neighbor destination)
+      board)))
